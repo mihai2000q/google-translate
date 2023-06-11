d8bf901995e8998d7b7e8b8f72b5de715070ea66
Chris <FilipCristiMihail@yahoo.com>
2023-06-11 12:21:43 +0200

    Initialize Custom Tkinter

---
+++ __app__.py
@@ -0,0 +1,7 @@
+from customtkinter import *
+
+
+class App(CTk):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.mainloop()
---
+++ __main__.py
@@ -0,0 +1,9 @@
+from __app__ import App
+
+
+def main():
+    App()
+
+
+if __name__ == '__main__':
+    main()
